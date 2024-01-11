170323867452f2ff0b8d35c40c8ef7fe38dc2620
Che' J. Holloway <chejholloway@outlook.com>
2024-01-11 10:48:40 -0800

    chore(global): :heavy_minus_sign: removes .vscode and .env from github tracking

--- src/app/components/BoardImage.tsx
+++ src/app/components/BoardImage.tsx
@@ -4,11 +4,11 @@ import Image from 'next/image'
 import BoardImageProps from '../interfaces/BoardImageProps.ts'
 
 const BoardImage: FC<BoardImageProps> = ({ src, alt }) => (
-  <figure className="mb-4 mx-2 relative group w-[210px] h-[210px] rounded-lg overflow-hidden border-gray-100 border-0 opacity-0 hover:border-8 opacity-100 transition-all duration-300 ">
+  <figure className="mb-4 mx-2 relative group w-[210px] h-[210px] rounded-lg overflow-hidden border-gray-200 border-0 opacity-0 hover:border-8 opacity-100 transition-all duration-200 ">
     <div
-      className="w-full h-full bg-cover bg-center rounded-lg relative "
+      className="w-full h-full bg-cover bg-center rounded-lg relative cursor-pointer"
       style={{ backgroundImage: `url(${src})` }}>
-      <div class="absolute cursor-pointer z-100 top-2 right-2 text-center hidden group-hover:block rounded-lg bg-black text-white flex justify-center items-center w-8 h-8 opacity-75">
+      <div class="absolute z-100 top-2 right-2 text-center hidden group-hover:block rounded-lg bg-black text-white flex justify-center items-center w-8 h-8 opacity-75">
         ...
       </div>
       <div className="absolute inset-0  bg-gradient-to-t from-black to-white opacity-50 rounded-lg"></div>
