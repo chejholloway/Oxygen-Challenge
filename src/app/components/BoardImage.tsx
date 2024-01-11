1f3468de53efe970f527a946ad8f0bca20344237
Che' J. Holloway <chejholloway@outlook.com>
2024-01-11 09:56:48 -0800

    feat(boards): :art: adds border on hover
    removes export from nextjs.config

--- src/app/components/BoardImage.tsx
+++ src/app/components/BoardImage.tsx
@@ -4,9 +4,9 @@ import Image from 'next/image'
 import BoardImageProps from '../interfaces/BoardImageProps.ts'
 
 const BoardImage: FC<BoardImageProps> = ({ src, alt }) => (
-  <figure className="mb-4 mx-2 relative group w-[210px] h-[210px] rounded-lg overflow-hidden">
+  <figure className="mb-4 mx-2 relative group w-[210px] h-[210px] rounded-lg overflow-hidden border-gray-100 border-0 opacity-0 hover:border-8 opacity-100 transition-all duration-300 ">
     <div
-      className="w-full h-full bg-cover bg-center rounded-lg relative"
+      className="w-full h-full bg-cover bg-center rounded-lg relative "
       style={{ backgroundImage: `url(${src})` }}>
       <div class="absolute cursor-pointer z-100 top-2 right-2 text-center hidden group-hover:block rounded-lg bg-black text-white flex justify-center items-center w-8 h-8 opacity-75">
         ...
