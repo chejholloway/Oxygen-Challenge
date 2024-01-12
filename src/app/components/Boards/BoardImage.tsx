801dee4fcf62f4e774d95bb07bc1e1f9a3e9c440
Che' J. Holloway <chejholloway@outlook.com>
2024-01-12 14:17:07 -0800

    refactor(global): :art: externalizes styles using twin.macro
    creates caption and ellipses components

--- src/app/components/Boards/BoardImage.tsx
+++ src/app/components/Boards/BoardImage.tsx
@@ -1,21 +1,23 @@
 // components/BoardImage.tsx
 import { FC } from 'react'
 import Image from 'next/image'
+
 import BoardImageProps from '../../interfaces/BoardImageProps.js'
+import {
+  Ellipses,
+  FadeEffect,
+  Figure,
+  ImageContainer,
+  Title,
+} from './styles/Boards.styles'
 
 const BoardImage: FC<BoardImageProps> = ({ src, alt }) => (
-  <figure className="mb-4 mx-2 relative group w-[210px] h-[210px] rounded-lg overflow-hidden border-gray-200 border-0 opacity-0 hover:border-8 opacity-100 transition-all duration-200 ">
-    <div
-      className="w-full h-full bg-cover bg-center rounded-lg relative cursor-pointer"
-      style={{ backgroundImage: `url(${src})` }}>
-      <div className="absolute z-100 top-2 right-2 text-center hidden group-hover:block rounded-lg bg-black text-white flex justify-center items-center w-8 h-8 opacity-75">
-        ...
-      </div>
-      <div className="absolute inset-0  bg-gradient-to-t from-black to-white opacity-50 rounded-lg"></div>
-      <h3 className="absolute bottom-0 left-0 p-4 text-white font-sans text-xl antialiased font-semibold tracking-normal">
-        {alt}
-      </h3>
-    </div>
+  <figure className={Figure}>
+    <ImageContainer style={{ backgroundImage: `url(${src})` }}>
+      <Ellipses>...</Ellipses>
+      <FadeEffect />
+      <Title>{alt}</Title>
+    </ImageContainer>
   </figure>
 )
 
--- src/app/components/Boards/styles/Boards.styles.tsx
+++ src/app/components/Boards/styles/Boards.styles.tsx
@@ -6,3 +6,20 @@ export const BoardsContainer = styled.div`
 export const BoardsImageList = styled.div`
   ${tw`flex flex-wrap pt-6`}
 `
+export const Ellipses = styled.span`
+  ${tw`absolute z-10 top-2 right-2 text-center hidden group-hover:block rounded-lg bg-black text-white flex justify-center items-center w-8 h-8 opacity-75`}
+`
+
+export const ImageContainer = styled.div`
+  ${tw`w-full h-full bg-cover bg-center rounded-lg relative cursor-pointer`}
+`
+
+export const FadeEffect = styled.div`
+  ${tw`absolute bottom-0 left-0 p-4 text-white font-sans text-xl antialiased font-semibold tracking-normal`}
+`
+
+export const Title = styled.h3`
+  ${tw`absolute bottom-0 left-0 p-4 text-white font-sans text-xl antialiased font-semibold tracking-normal`}
+`
+
+export const Figure = `mb-4 mx-2 relative group w-[210px] h-[210px] rounded-lg overflow-hidden border-gray-200 border-0 opacity-0 hover:border-8 opacity-100 transition-all duration-200`
